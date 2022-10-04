7d29f24bc5b5b4e1d590e15df2dff83c85da0ac7
Thiago Balonyi <thiagocandal@gmail.com>
2022-10-04 15:27:25 -0300

    Atualizações de estilo via js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const form = document.getElementById('form-deposito');
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
+let formEvalido = false;
 
 function validaNome(nomeCompleto) {
     const nomeComoArray = nomeCompleto.split(' ');
@@ -6,27 +8,38 @@ function validaNome(nomeCompleto) {
 } //Aqui essa função é para validar nome com 2 nomes//
 
 form.addEventListener('submit', function(e) {
-    let formEvalido = false
     e.preventDefault(); //este código serve para tirar o reload do formulário//
 
-    const nomeBeneficiario = document.getElementById('nome-beneficiario');
     const numeroContaBeneficiario = document.getElementById('numero-conta')
     const valorDeposito = document.getElementById('valor-deposito')
-    //recuperar outras infos//
-
-    const mensagemSucesso = `Montante de: ${valorDeposito.value} deposito para o cliente: ${nomeBeneficiario.value} - conta: ${numeroContaBeneficiario.value}`;
+    const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> deposito para o cliente: <b>${nomeBeneficiario.value}</b> - conta: <b>${numeroContaBeneficiario.value}</b>`;
 
     formEvalido = validaNome(nomeBeneficiario.value)
     if (formEvalido) {
-        alert(mensagemSucesso)
+        const containerMensagemSucesso = document.querySelector('.sucess-message');
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
 
         nomeBeneficiario.value = '';
         numeroContaBeneficiario.value = '';
         valorDeposito.value = '';
 
     } else {
-        alert('Nome incompleto')
+        nomeBeneficiario.style.border = '1px solid red';
+        document.querySelector('.error-message').style.display = 'block';
     }//este código serve para alertar nome incompleto ou deposito feito//
 })
 
-console.log(form);
+nomeBeneficiario.addEventListener('keyup', function(e) {
+    console.log(e.target.value);
+    formEvalido = validaNome(e.target.value);
+
+    if (!formEvalido) {
+        nomeBeneficiario.classList.add('error');
+        //nomeBeneficiario.style.border = '1px solid red';
+        document.querySelector('.error-message').style.display = 'block';
+    } else {
+        nomeBeneficiario.classList.remove('error');
+        document.querySelector('.error-message').style.display = 'none';
+    }
+});
